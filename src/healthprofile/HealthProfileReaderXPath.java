cf19f1a7c869f72de84ca96538baf114aca558a9
djbb7 <danielbruzual@gmail.com>
2015-10-17 22:28:51 +0200

    Add comments

--- src/healthprofile/HealthProfileReaderXPath.java
+++ src/healthprofile/HealthProfileReaderXPath.java
@@ -28,11 +28,17 @@ public class HealthProfileReaderXPath {
 	public static String XML_LOCATION;
 
 	public HealthProfileReaderXPath(){
+		//For proper parsing of decimal numbers
 		Locale.setDefault(Locale.US);
 	}
 
+	/**
+	 * Parse XML document containing people database
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 * @throws IOException
+	 */
 	public void loadXML() throws ParserConfigurationException, SAXException, IOException {
-
 		DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
 		domFactory.setNamespaceAware(true);
 		DocumentBuilder builder = domFactory.newDocumentBuilder();
@@ -42,29 +48,50 @@ public class HealthProfileReaderXPath {
 		getXPathObj();
 	}
 
+	/**
+	 * @return XPath object to traverse the XML file
+	 */
 	public XPath getXPathObj() {
-
 		XPathFactory factory = XPathFactory.newInstance();
 		xpath = factory.newXPath();
 		return xpath;
 	}
 
+	/**
+	 * Find a person by full name.
+	 * @param firstname
+	 * @param lastname
+	 * @return The XML node corresponding to the person, or null if not found.
+	 * @throws XPathExpressionException
+	 */
 	public Node getPersonByFullname(String firstname, String lastname) throws XPathExpressionException {
 		XPathExpression expr = xpath.compile("/people/person[firstname='" + firstname + "' and lastname='"+lastname+"']");
 		Node node = (Node) expr.evaluate(doc, XPathConstants.NODE);
 		return node;
 	}
 
+	/**
+	 * Find a person's Health Profile.
+	 * @param id
+	 * @return The XML node corresponding to the Health Profile, or null if not found.
+	 * @throws XPathExpressionException
+	 */
 	public Node findHealthProfileByPersonId(int id) throws XPathExpressionException{
 		XPathExpression expr = xpath.compile("/people/person[@id=" + id+"]/healthprofile");
 		Node hProfile = (Node) expr.evaluate(doc, XPathConstants.NODE);
 		return hProfile;
 	}
 
+	/**
+	 * Find a person's full name
+	 * @param id
+	 * @return A String containing the person's full name in <lastname, firstname> format, or null if not found.
+	 * @throws XPathExpressionException
+	 */
 	public String getFullnameById (int id) throws XPathExpressionException{
 		XPathExpression expr = xpath.compile("//person[@id="+id+"]/firstname | //person[@id="+id+"]/lastname");
 		NodeList names = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
-		String firstname = "", lastname = "";
+		String firstname=null, lastname=null;
 		for(int j=0; j<names.getLength(); j++){
 			Node name = names.item(j);
 			if(name.getNodeName().equals("firstname")){
@@ -73,27 +100,58 @@ public class HealthProfileReaderXPath {
 				lastname = name.getTextContent();
 			}
 		}
+		if(firstname==null && lastname==null)
+			return null;
+		
 		return lastname+", "+firstname;
 	}
 
+	/**
+	 * Get the weight of a person by id.
+	 * @param id
+	 * @return The weight, or null if not found.
+	 * @throws XPathExpressionException
+	 */
 	public Double getWeight(int id) throws XPathExpressionException{
 		XPathExpression expr = xpath.compile("//person[@id="+id+"]/weight");
 		String res = (String) expr.evaluate(doc, XPathConstants.STRING);
+		if(res == null) 
+			return null;
 		return Double.valueOf(res);
 	}
 
+	/**
+	 * Get the weight of a person by full name.
+	 * @param firstname
+	 * @param lastname
+	 * @return The height, or null if not found.
+	 * @throws XPathExpressionException
+	 */
 	public Double getWeight(String firstname, String lastname) throws XPathExpressionException{
 		XPathExpression expr = xpath.compile("//person[firstname='"+firstname+"' and lastname='"+lastname+"']/weight");
 		String res = (String) expr.evaluate(doc, XPathConstants.STRING);
 		return Double.valueOf(res);
 	}
 
+	/**
+	 * Get the height of a person by id.
+	 * @param id
+	 * @return The weight, or null if not found.
+	 * @throws XPathExpressionException
+	 */
 	public Double getHeight(int id) throws XPathExpressionException{
 		XPathExpression expr = xpath.compile("//person[@id="+id+"]/weight");
 		String res = (String) expr.evaluate(doc, XPathConstants.STRING);
 		return Double.valueOf(res);
 	}
 
+	/**
+	 * Get the height of a person by full name.
+	 * @param firstname
+	 * @param lastname
+	 * @return The height, or null if not found.
+	 * @throws XPathExpressionException
+	 */
 	public Double getHeight(String firstname, String lastname) throws XPathExpressionException{
 		XPathExpression expr = xpath.compile("//person[firstname='"+firstname+"' and lastname='"+lastname+"']/weight");
 		String res = (String) expr.evaluate(doc, XPathConstants.STRING);
@@ -101,6 +159,13 @@ public class HealthProfileReaderXPath {
 	}
 
 
+	/**
+	 * Find all the people who's weight satisfies the condition.
+	 * @param op Comparison operator: '<', '>', '='
+	 * @param weight Value to compare
+	 * @return All the matching people, or null if no match.
+	 * @throws XPathExpressionException
+	 */
 	public NodeList getPersonsByWeight(String op, Double weight) throws XPathExpressionException {
 		//do the actual searching now
 		XPathExpression expr = xpath.compile(String.format("/people/person[./healthprofile/weight %s %.2f]",op,weight.doubleValue()));
@@ -109,17 +174,22 @@ public class HealthProfileReaderXPath {
 
 	}
 
+	/**
+	 * Find all people in the database
+	 * @return
+	 * @throws XPathExpressionException
+	 */
 	public NodeList getAllPeople() throws XPathExpressionException {
 		XPathExpression expr = xpath.compile("//person");
 		return (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
 	}
 
 
+	/**
+	 * Pretty print, in table format, all the people in the database.
+	 */
 	public void printAllPeople(){
-		System.out.println ("--------------------------------------------------------------------------------------------------------------------");
-		System.out.format("%5s%20s%20s%15s%15s%15s%10s%10s", "ID", "First Name", "Last Name", "Birthdate", "Last Update", "Weight", "Height", "BMI");
-		System.out.println ("\n--------------------------------------------------------------------------------------------------------------------");
-
+		printTableHeader();
 		try {
 			NodeList persons = getAllPeople();
 			printPeople(persons);
@@ -128,6 +198,18 @@ public class HealthProfileReaderXPath {
 		}
 	}
 
+	/**
+	 * Print table headers for pretty print
+	 */
+	private void printTableHeader(){
+		System.out.println ("--------------------------------------------------------------------------------------------------------------------");
+		System.out.format("%5s%20s%20s%15s%15s%15s%10s%10s", "ID", "First Name", "Last Name", "Birthdate", "Last Update", "Weight", "Height", "BMI");
+		System.out.println ("\n--------------------------------------------------------------------------------------------------------------------");
+	}
+	
+	/**
+	 * Print contents of table
+	 */
 	private void printPeople(NodeList people){
 		for(int k=0; k<people.getLength(); k++){
 			Node person = people.item(k);
@@ -135,6 +217,10 @@ public class HealthProfileReaderXPath {
 		}
 	}
 
+	/**
+	 * Print a person as a row with his/her details.
+	 * @param person
+	 */
 	private void printPerson(Node person){
 		String id="", first="", last="", dob="", update="", bmi="", weight="", height="";
 		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
@@ -179,6 +265,10 @@ public class HealthProfileReaderXPath {
 		System.out.println(String.format("%5s%20s%20s%15s%15s%15s%10s%10s", id, first, last, dob, update, weight, height, bmi));
 	}
 
+	/**
+	 * Print all the people who match the condition
+	 * @param query Represents a condition for comparing weight (e.g. '<90').
+	 */
 	public void printPeopleByWeight(String query){
 		String op = query.substring(0, 1);
 		if(!op.equals(">") && !op.equals("<") && !op.equals("=")) {
@@ -197,10 +287,8 @@ public class HealthProfileReaderXPath {
 		try {
 			System.out.println(String.format("People with weight%s%.2fkg",op,weight));
 
-			System.out.println ("\n--------------------------------------------------------------------------------------------------------------------");
-			System.out.format("%5s%20s%20s%15s%15s%15s%10s%10s", "ID", "First Name", "Last Name", "Birthdate", "Last Update", "Weight", "Height", "BMI");
-			System.out.println ("\n--------------------------------------------------------------------------------------------------------------------");
-
+			printTableHeader();
+			
 			NodeList people = getPersonsByWeight(op, weight);
 			printPeople(people);
 		} catch (XPathExpressionException e){
@@ -208,6 +296,10 @@ public class HealthProfileReaderXPath {
 		}
 	}
 
+	/**
+	 * Print the health profile for a given person.
+	 * @param idPerson
+	 */
 	public void printHealthProfile(int idPerson){
 		String update="", weight="", height="", bmi="";
 		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
@@ -257,16 +349,15 @@ public class HealthProfileReaderXPath {
 	 */
 	public static void main(String[] args) throws ParserConfigurationException, SAXException,
 	IOException, XPathExpressionException {
+		if(args.length <2){
+			System.out.println("Please specify xml location and command to be executed.");
+			System.out.println("Example: java HealthProfileReaderXPath xml/people.xml printAll");
+			System.out.println("\nValid commands: printAll, printHealthProfile <id>, printByWeight '<condition>'");
+			return;
+		}
 		HealthProfileReaderXPath.XML_LOCATION = args[0];
 		String command = args[1];
-		if(HealthProfileReaderXPath.XML_LOCATION == null){
-			System.err.println("No XML file specified");
-			return;
-		}
-		if(command==null){
-			System.err.println("No command specified.");
-			return;
-		}
+
 
 		HealthProfileReaderXPath reader = new HealthProfileReaderXPath();
 		reader.loadXML();
